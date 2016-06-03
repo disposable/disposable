54dc1692ed773baa3dba187a4ea12fa84c051ea1
Andrei Simionescu <ansimionescu@gmail.com>
2016-06-03 02:03:26 +0200

    Added node code

---
+++ disposable.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var domainMap = {};
+for (let d of require('./domains')) {
+    domainMap[d] = null;
+}
+
+module.exports = {
+    validate: (domain, callback) => {
+        if (!callback) {
+            return domainMap.hasOwnProperty(domain);
+        }
+        callback(null, domainMap.hasOwnProperty(domain));
+    }
+}
