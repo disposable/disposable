19011473838bf8ef66f45ef1fa9f2622c16d3292
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-08-05 13:39:06 +0200

    auto update

--- disposable.js
+++ disposable.js
@@ -1,19 +1,66 @@
 'use strict';
 
-var domainMap = {}
-var arr = require('./domains')
+const fs = require('fs');
+const { execSync } = require('child_process')
+const path = require('path');
 
-for (var i = 0; i < arr.length; ++i)
-    domainMap[arr[i]] = null
+const domainJson = path.join(__dirname, 'disposable.js');
+
+let domainMap = {}
+
+function shouldUpdateDomains() {
+    const fileExists = fs.existsSync(domainJson);
+
+    if (!fileExists) {
+        return true;
+    }
+
+    const stat = fs.statSync(domainJson);
+
+    if (stat.mtimeMs < (new Date().getTime() - 1000 * 60 * 60 * 24)) {
+        return true;
+    }
+
+    return false;
+}
+
+function updateDomains() {
+    try {
+        execSync(path.join(__dirname, 'update.sh'))
+    } catch (err) {
+        console.error('Error updating domains')
+        console.error(err.message)
+    }
+}
+
+function loadDomains() {
+    const arr = require('./domains')
+    domainMap = {}
+
+    for (let i = 0; i < arr.length; ++i)
+        domainMap[arr[i]] = null
+}
+
+if (shouldUpdateDomains()) {
+    updateDomains()
+}
+
+loadDomains()
 
 module.exports = {
-    validate: function(domainOrEmail, callback) {
-        var domain = domainOrEmail.split('@').pop()
-        var isValid = !domainMap.hasOwnProperty(domain)
+    validate: function (domainOrEmail, callback) {
+        if (shouldUpdateDomains()) {
+            updateDomains()
+            loadDomains()
+        }
+        
+        const domain = domainOrEmail.split('@').pop()
+        const isValid = !domainMap.hasOwnProperty(domain)
 
         if (!callback) {
             return isValid
         }
+
         callback(null, isValid)
     }
 }
---
+++ update.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains.json --output domains.json
